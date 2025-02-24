37a4c6ad48c80dc5ebe8319ef0b7352a3aaa2cd6
liangxiansheng093 <114294415+liangxiansheng093@users.noreply.github.com>
2025-02-24 14:25:28 +0800

    Update BoRe_Depth.py

--- model/BoRe_Depth.py
+++ model/BoRe_Depth.py
@@ -3,11 +3,6 @@ from pytorch_lightning import LightningModule
 
 from .DepthNet import DepthNet
 from .PoseNet import PoseNet
-from .Seg_Encoder import Seg_Encoder
-
-
-seg_model = Seg_Encoder()
-seg_model.load_state_dict(torch.load("checkpoints/semantic.pth")["state_dict"], strict=False)
 
 
 class BoRe_Depth(LightningModule):
